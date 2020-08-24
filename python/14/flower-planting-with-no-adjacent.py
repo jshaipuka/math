4d66e99d3d7ac3056405c247e178d4cc9161f7e8
jelena <jelena.shaipuka@gmail.com>
2020-08-24 20:19:19 +0300

    14

--- python/14/flower-planting-with-no-adjacent.py
+++ python/14/flower-planting-with-no-adjacent.py
@@ -1,32 +1,42 @@
-# https://leetcode.com/problems/flower-planting-with-no-adjacent
-
-# Pochti rabotaet
-# Input
-# 5
-# [[3,4],[4,5],[3,2],[5,1],[1,3],[4,2]]
-# Output
-# [1,4,1,2,3]
-# Expected
-# [1,1,2,3,2]
-
 class Solution:
+    
+    def remove(self, arr, a):
+        if a in arr:
+            arr.remove(a)
+        
     def gardenNoAdj(self, N: int, paths: List[List[int]]) -> List[int]:
         planted = [0 for garden in range(N)]
-        flowers = [1,2,3,4]
-        flower_index = 0
+        colored = [[1,2,3,4] for garden in range(N)]
 
         for path in paths:
                 [garden_from, garden_to] = path
+                
+                if planted[garden_from - 1] == 0 and planted[garden_to - 1] == 0:
+                    # init
+                    planted[garden_from - 1] = 1
+                    planted[garden_to - 1] = 2
+                    colored[garden_from - 1].remove(2)
+                    colored[garden_to - 1].remove(1)
+                    
+                elif planted[garden_from - 1] == 0 and planted[garden_to - 1] != 0:
+                    self.remove(colored[garden_from - 1], planted[garden_to - 1])
+                    planted[garden_from - 1] = colored[garden_from - 1][0]
+                    self.remove(colored[garden_to - 1], planted[garden_from - 1])
+                elif planted[garden_from - 1] != 0 and planted[garden_to - 1] == 0:
+                    self.remove(colored[garden_to - 1], planted[garden_from - 1])
+                    planted[garden_to - 1] = colored[garden_to - 1][0]
+                    self.remove(colored[garden_from - 1], planted[garden_to - 1])
+                elif planted[garden_from - 1] != 0 and planted[garden_to - 1] != 0 and planted[garden_from - 1] == planted[garden_to - 1]:
+                    # same flower in both gardens
+                    self.remove(colored[garden_from - 1], planted[garden_to - 1])
+                    planted[garden_from - 1] = colored[garden_from - 1][0]
+                    
+                    
 
-                if planted[garden_from - 1] == 0:
-                    planted[garden_from - 1] = flowers[flower_index]
-                    flower_index = flower_index + 1 if flower_index + 1 < 4 else 0
-                if planted[garden_to - 1] == 0:
-                    planted[garden_to - 1] = flowers[flower_index]
-                    flower_index = flower_index + 1 if flower_index + 1 < 4 else 0
-        
         for i in range(len(planted)):
             if planted[i] == 0:
                 planted[i] = 1
         
         return planted
+                    
+            
